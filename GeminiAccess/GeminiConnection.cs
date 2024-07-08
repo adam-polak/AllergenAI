4b316245805d4451492e000f7201ba300e9898fa
Adam Polak <adamjpolak@gmail.com>
2024-07-08 10:00:18 -0500

    hide appsettings

--- GeminiAccess/GeminiConnection.cs
+++ GeminiAccess/GeminiConnection.cs
@@ -1,26 +1,21 @@
 using RestSharp;
 using Newtonsoft.Json;
-using GenerativeAI.Models;
+using Junaid.GoogleGemini.Net;
 
 namespace AllergenAI.GeminiAccess;
 
 public class GeminiConnection
 {
     private readonly string apiKey;
-    GenerativeModel model;
     IConfiguration _configuration;
 
     public GeminiConnection(IConfiguration configuration)
     {
         _configuration = configuration;
-        apiKey = _configuration.GetConnectionString("Key") ?? "";
-        model = new GenerativeModel(apiKey);
     }
 
     public async void CheckForAllergens(string food, List<string> allergens)
     {
-        var result = await model.GenerateContentAsync($"Does {food} contain peanuts?");
-        Console.WriteLine(result);
     }
 
 }
